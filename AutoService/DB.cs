44153ccfbb4069a454cc2ff48f292b8b5c7a2ab1
Ahmed Gadjiev <akhmed.34@mail.ru>
2024-03-30 13:44:40 +0300

    lab2

---
+++ AutoService/DB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoService
+{
+    internal class DB
+    {
+        public static AutoServiceEntities Connection = new AutoServiceEntities();
+    }
+
+
+}
